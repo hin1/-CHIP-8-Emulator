660636e471ee272f6d14685a5f89641a0ee129d4
hin1 <sean.chanhin1@gmail.com>
2019-09-26 16:47:36 +0800

    Defined CHIP-8 class with components, and created basic function to load ROM contents in emulator

---
+++ chip8.cpp
@@ -0,0 +1,59 @@
+#include <cstdint>
+#include <fstream>
+
+const unsigned int START_ADDRESS = 0x200;
+
+class Chip8
+{
+  public:
+
+    //Different components to emulate
+    uint8_t registers[16]{};
+    uint8_t memory[4096]{};
+    uint16_t index{};
+    uint16_t pc{};
+    uint16_t stack[16]{};
+    uint8_t sp{};
+    uint8_t delayTimer{};
+    uint8_t soundTimer{};
+    uint8_t keypad[16]{};
+    uint32_t video[64 * 32]{};
+    uint16_t opcode;
+
+    //Constructor
+    Chip8() {
+      // Initialize PC
+      pc = START_ADDRESS;
+    }
+    
+    //Member functions
+    void LoadROM(char const* filename) {
+      // Open the file as a stream of binary and move the file pointer to the end
+      std::ifstream file(filename, std::ios::binary | std::ios::ate);
+
+      if (file.is_open())
+      {
+        // Get size of file and allocate a buffer to hold the contents
+        std::streampos size = file.tellg();
+        char* buffer = new char[size];
+
+        // Go back to the beginning of the file and fill the buffer
+        file.seekg(0, std::ios::beg);
+        file.read(buffer, size);
+        file.close();
+
+        // Load the ROM contents into the Chip8's memory, starting at 0x200
+        for (long i = 0; i < size; ++i)
+        {
+          memory[START_ADDRESS + i] = buffer[i];
+        }
+
+        // Free the buffer
+        delete[] buffer;
+      }
+    }
+
+};
+
+
+
