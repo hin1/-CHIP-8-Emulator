92fdc7743486e6952c1a2f113b67ecf1c55f6c69
hin1 <sean.chanhin1@gmail.com>
2019-10-15 01:05:57 +0800

    Added opcodes up to SE Vx, byte

--- chip8.cpp
+++ chip8.cpp
@@ -1,9 +1,13 @@
-G#include <cstdint>
+#include <cstdint>
 #include <fstream>
+#include <chrono>
+#include <random>
 
 const unsigned int START_ADDRESS = 0x200;
 const unsigned int FONTSET_START_ADDRESS = 0x50;
 const unsigned int FONTSET_SIZE = 80;
+const unsigned int VIDEO_HEIGHT = 32;
+const unsigned int VIDEO_WIDTH = 64;
 
 //Sprites for characters
 uint8_t fontset[FONTSET_SIZE] =
@@ -26,25 +30,30 @@ uint8_t fontset[FONTSET_SIZE] =
   0xF0, 0x80, 0xF0, 0x80, 0x80  // F
 };
 
-class Chip8
-{
+class Chip8 {
+
   public:
 
     //Components of CHIP-8
-    uint8_t registers[16]{};
-    uint8_t memory[4096]{};
-    uint16_t index{};
-    uint16_t pc{};
-    uint16_t stack[16]{};
-    uint8_t sp{};
-    uint8_t delayTimer{};
-    uint8_t soundTimer{};
-    uint8_t keypad[16]{};
-    uint32_t video[64 * 32]{};
+    uint8_t registers[16];
+    uint8_t memory[4096];
+    uint16_t index;
+    uint16_t pc;
+    uint16_t stack[16];
+    uint8_t sp;
+    uint8_t delayTimer;
+    uint8_t soundTimer;
+    uint8_t keypad[16];
+    uint32_t video[VIDEO_WIDTH * VIDEO_HEIGHT];
     uint16_t opcode;
 
+    //Helper member variables
+    std::default_random_engine randGen;
+    std::uniform_int_distribution<uint8_t> randByte;
+
     //Constructor
-    Chip8() {
+    Chip8() : randGen(std::chrono::system_clock::now().time_since_epoch().count())
+    {
 
       // Initialize PC
       pc = START_ADDRESS;
@@ -52,12 +61,14 @@ class Chip8
       // Load fonts into memory
       for (unsigned int i = 0; i < FONTSET_SIZE; ++i)
       {
-        memory = [FONTSET_START_ADDRESS + i] = fontset[i];
+        memory[FONTSET_START_ADDRESS + i] = fontset[i];
       }
+
+      // Initialize RNG
+      randByte = std::uniform_int_distribution<uint8_t>(0, 255U);
 
     }
 
-    //Member functions
     void LoadROM(char const* filename) {
       // Open the file as a stream of binary and move the file pointer to the end
       std::ifstream file(filename, std::ios::binary | std::ios::ate);
@@ -83,8 +94,57 @@ class Chip8
         delete[] buffer;
       }
     }
+    
+    
+    /**
+     * 00E0: CLS
+     * Clears the display.
+     */
+    void OP_00E0() {
+      memset(video, 0, VIDEO_HEIGHT * VIDEO_WIDTH);
+    }
+    
+    /**
+     * 00EE: RET
+     * Returns from a subroutine.
+     */
+    void OP_00EE() {
+      --sp;
+      pc = stack[sp];
+    }
+
+    /**
+     * 1nnn: JP addr
+     * Jumps to location NNN.
+     */
+    void OP_1nnn() {
+      uint16_t address = opcode & 0x0FFFu; //bitmask to get location
+      pc = address;
+    }
+
+    /**
+     * 2nnn: CALL addr
+     * Calls subroutine at location nnn.
+     */
+    void OP_2nnn() {
+      uint16_t address = opcode & 0x0FFFu;
+      pc = address;
+      stack[sp] = pc;
+      ++sp;
+      pc = address;
+    }
+
+    /**
+     * 3xkk: SE Vx, byte
+     * Skips next instruction if Vx = kk.
+     */
+    void OP_3xkk() {
+      uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+      uint8_t byte = opcode & 0x00FFu;
+      if (registers[Vx] == byte) {
+        pc += 2;
+      }
+    }
+
 
 };
-
-
-
