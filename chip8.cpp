5e24003185653eb1b44bcbe84bb24eb8f53acca8
hin1 <sean.chanhin1@gmail.com>
2020-04-14 23:48:51 +0800

    Added instructions Dxyn to Fx29

--- chip8.cpp
+++ chip8.cpp
@@ -195,8 +195,8 @@ class Chip8 {
      * Sets register Vx with the value of register Vy.
      */
     void OP_8xy0() {
-      uint8_t Vx = (opcode & 0x0F00) >> 8u;
-      uint8_t Vy = (opcode & 0x00F0) >> 4u;
+      uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+      uint8_t Vy = (opcode & 0x00F0u) >> 4u;
       registers[Vx] = registers[Vy];
     }
 
@@ -206,8 +206,8 @@ class Chip8 {
      * result in Vx.
      */
     void OP_8xy1() {
-      uint8_t Vx = (opcode & 0x0F00) >> 8u;
-      uint8_t Vy = (opcode & 0x00F0) >> 4u;
+      uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+      uint8_t Vy = (opcode & 0x00F0u) >> 4u;
       registers[Vx] |= registers[Vy];
     }
 
@@ -217,8 +217,8 @@ class Chip8 {
      * result in Vx.
      */
     void OP_8xy2() {
-      uint8_t Vx = (opcode & 0x0F00) >> 8u;
-      uint8_t Vy = (opcode & 0x00F0) >> 4u;
+      uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+      uint8_t Vy = (opcode & 0x00F0u) >> 4u;
       registers[Vx] &= registers[Vy];
     } 
 
@@ -228,8 +228,8 @@ class Chip8 {
      * result in Vx.
      */
     void OP_8xy3() {
-      uint8_t Vx = (opcode & 0x0F00) >> 8u;
-      uint8_t Vy = (opcode & 0x00F0) >> 4u;
+      uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+      uint8_t Vy = (opcode & 0x00F0u) >> 4u;
       registers[Vx] ^= registers[Vy];
     }
 
@@ -240,8 +240,8 @@ class Chip8 {
      * set to 1, otherwise 0.
      */
     void OP_8xy4() {
-      uint8_t Vx = (opcode & 0x0F00) >> 8u;
-      uint8_t Vy = (opcode & 0x00F0) >> 4u;
+      uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+      uint8_t Vy = (opcode & 0x00F0u) >> 4u;
       uint16_t result = registers[Vx] + registers[Vy];
       if (result > 255u){
         registers[15] = 1;
@@ -259,15 +259,14 @@ class Chip8 {
      * set to 1, otherwise 0.
      */
     void OP_8xy5() {
-      uint8_t Vx = (opcode & 0x0F00) >> 8u;
-      uint8_t Vy = (opcode & 0x00F0) >> 4u;
+      uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+      uint8_t Vy = (opcode & 0x00F0u) >> 4u;
       if (registers[Vx] > registers[Vy]){
         registers[15] = 1;
       } else {
         registers[15] = 0;
       }
       registers[Vx] -= registers[Vy];
-      //registers[Vx] = result & 0xFFu;
     }
 
     /**
@@ -276,13 +275,233 @@ class Chip8 {
      * Vx. Set register VF to value of LSB of Vy before the shift.
      */
     void OP_8xy6() {
-      uint8_t Vx = (opcode & 0x0F00) >> 8u;
-      uint8_t Vy = (opcode & 0x00F0) >> 4u;
+      uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+      uint8_t Vy = (opcode & 0x00F0u) >> 4u;
       registers[15] = (registers[Vy] & 0x1u);
       registers[Vx] = (registers[Vy]>>1);
     } 
 
+    /**
+     * 8xy7: SUBN Vx, Vy
+     * Subtracts value of registers Vx from Vy and stores the result in Vx.
+     * If value of Vy is more than Vx, register VF (16) is 
+     * set to 1, otherwise 0.
+     */
+    void OP_8xy7() {
+      uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+      uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+      if (registers[Vy] > registers[Vx]){
+        registers[15] = 1;
+      } else {
+        registers[15] = 0;
+      }
+      registers[Vx] = registers[Vy] - registers[Vx];
+    }
 
+    /**
+     * 8xyE: SHL Vx, Vy
+     * Store value of register Vy shifted left by one bit in register
+     * Vx. Set register VF to value of MSB of Vy before the shift.
+     */
+    void OP_8xyE() {
+      uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+      uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+      registers[15] = (registers[Vy] & 0xF0u) >> 7u;
+      registers[Vx] = (registers[Vy]<<1);
+    } 
+
+    /**
+     * 9xy0: SNE Vx, Vy
+     * Skips next instruction if Vx != Vy.
+     */
+    void OP_9xy0() {
+      uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+      uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+      if (registers[Vx] != registers[Vy]) {
+        pc += 2;
+      }
+    }
+
+    /**
+     * Annn: LD I, addr
+     * Set index register I as location nnn.
+     */
+    void OP_Annn() {
+      uint16_t address = opcode & 0x0FFFu;
+      index = address;
+    }
+
+    /**
+     * Bnnn: JP V0, addr
+     * Jumps to location nnn with offset stipulated by value of register V0.
+     */
+    void OP_Bnnn() {
+      uint16_t address = opcode & 0x0FFFu;
+      pc = address + registers[0];
+    }
+
+    /**
+     * Cxkk: RND Vx, byte
+     * Set value of register Vx as a random 8-bit number ANDed with
+     * number kk.
+     */
+    void OP_Cxkk() {
+      uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+      uint8_t byte = (opcode & 0x00FFu);
+      registers[Vx] = (randByte(randGen) & byte);
+    }
+    
+    /**
+     * Dxyn: DRW Vx, Vy, nibble
+     * Draw sprite at position Vx, Vy, with n bytes of sprite data,
+     * starting at the address stored in the index register I. 
+     * Sets register VF to 1 if any set pixels are change to unset,
+     * 0 otherwise.
+     */
+    void OP_Dxyn() {
+      uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+      uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+      uint8_t height = opcode & 0x000Fu;
+
+      uint8_t xPos = registers[Vx] % VIDEO_WIDTH;
+      uint8_t yPos = registers[Vy] % VIDEO_HEIGHT;
+
+      registers[15] = 0;
+
+      for (unsigned int row = 0; row < height; row++) {
+        uint8_t spriteByte = memory[index + row];
+
+        for (unsigned int col = 0; col < 8; col++) {
+          uint8_t spritePixel = spriteByte & (0x80u >> col);
+          uint32_t* screenPixel = &video[(yPos + row) * VIDEO_WIDTH];
+
+          if (spritePixel) { //Sprite pixel is set
+            if (*screenPixel == 0xFFFFFFFF) { //Screen pixel also set - collision
+              registers[15] = 1;
+            }
+            
+          } 
+
+          //XOR with sprite pixel since it is now set
+          *screenPixel ^= 0xFFFFFFFF;
+
+        }
+      }
+    }
+
+     /**
+     * Ex9E: SKP Vx
+     * Skip next instruction if key with the value of Vx is pressed.
+     */
+    void OP_Ex9E() {
+      uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+      uint8_t key = registers[Vx];
+      if (keypad[key]) {
+        pc += 2;
+      }
+    }
+
+    /**
+     * ExA1: SKNP VX
+     * Skip next instruction if key with the value of Vx is not pressed.
+     */
+    void OP_ExA1() {
+      uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+      uint8_t key = registers[Vx];
+      if (!keypad[key]) {
+        pc += 2;
+      }
+    }
+
+    /**
+     * Fx07: LD Vx, DT
+     * Set Vx = delay timer value
+     */
+    void OP_Fx07() {
+      uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+      registers[Vx] = delayTimer;
+    }
+    
+    /**
+     * Fx0A: LD Vx, K
+     * Wait for a key press, store the value of the key in Vx.
+     */
+    void OP_Fx0A() {
+      uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+
+      if (keypad[0]) {
+        registers[Vx] = 0;
+      } else if (keypad[1]) {
+        registers[Vx] = 1;
+      } else if (keypad[2]) {
+        registers[Vx] = 2;
+      } else if (keypad[3]) {
+        registers[Vx] = 3;
+      } else if (keypad[4]) {
+        registers[Vx] = 4;
+      } else if (keypad[5]) {
+        registers[Vx] = 5;
+      } else if (keypad[6]) {
+        registers[Vx] = 6;
+      } else if (keypad[7]) {
+        registers[Vx] = 7;
+      } else if (keypad[8]) {
+        registers[Vx] = 8;
+      } else if (keypad[9]) {
+        registers[Vx] = 9;
+      } else if (keypad[10]) {
+        registers[Vx] = 10;
+      } else if (keypad[11]) {
+        registers[Vx] = 11;
+      } else if (keypad[12]) {
+        registers[Vx] = 12;
+      } else if (keypad[13]) {
+        registers[Vx] = 13;
+      } else if (keypad[14]) {
+        registers[Vx] = 14;
+      } else if (keypad[15]) {
+        registers[Vx] = 15;
+      } else {
+        pc -= 2;
+      }
+    }
+
+    /**
+     * Fx15: LD DT, Vx
+     * Set delay timer = Vx.
+     */
+    void OP_Fx15() {
+      uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+      delayTimer = registers[Vx];
+    }
+
+    /**
+     * Fx18: LD ST, Vx
+     * Set sound timer = Vx.
+     */
+    void OP_Fx18() {
+      uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+      soundTimer = registers[Vx];
+    }  
+
+    /**
+     * Fx1E: ADD I, Vx
+     * Set I = I + Vx.
+     */
+    void OP_Fx1E() {
+      uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+      index += registers[Vx];
+    }
+
+    /**
+     * Fx29: LD F, Vx
+     * Set I = location of sprite for digit Vx
+     */
+    void OP_Fx29() {
+      uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+      uint8_t digit = registers[Vx];
+      index += FONTSET_START_ADDRESS + (5 * digit);
+    }
 
 
 };
